a7a35412c071dd47d0dc349cbcbaaa2251a33ee0
avbprogramming <avb.programming@gmail.com>
2021-11-02 22:34:44 +0300

    final edition

--- src/main/java/Levenshtein.java
+++ src/main/java/Levenshtein.java
@@ -1,43 +1,63 @@
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.nio.Buffer;
 import java.util.*;
 
 public class Levenshtein {
 
-    public static void main (String[] args){
+    public static void main (String[] args) throws IOException {
         System.out.println("Левенштейн");
         Scanner scanner = new Scanner(System.in);
         System.out.println("Введите первое слово: ");
         String s1 = scanner.nextLine();
         System.out.println("Введите второе слово: ");
         String s2 = scanner.nextLine();
+        System.out.println("Какое количество раз вы хотите проверить скорость выполнения функции: ");
+        int count = Integer.parseInt(scanner.nextLine());
 
         int[][] dp = new int[s1.length() + 1][s2.length() + 1];
         for(int i = 0; i < s1.length() + 1; i++) {
             Arrays.fill(dp[i], -1);
         }
-        //jk
-        for (int i = 0 ; i <10; i++ ) {
-            long currentTime = System.currentTimeMillis();
-      //  System.out.println("Левенштейн с памятью и рекурсией " + RecursionWithMemory.minDis(s1, s2, s1.length(), s2.length(), dp));
-          System.out.println("Время выполнения без рекурсии " + LevenshteinMemory.computeLevenshteinDistanceDP(s1, s2));
-            long finalTime = System.currentTimeMillis()-currentTime;
-         System.out.println(finalTime);
+        System.out.println("Расстояние преобразования слов: " + LevenshteinMemory.computeLevenshteinDistanceDP(s1, s2));
+        System.out.println("Скорость выполнения каждого метода записаны в файл.");
+
+        long RecursionCurrentTime = System.nanoTime();
+        for (int i = 0 ; i <count; i++ ) {
+            Recursion.computeLevenshteinDistance(s1, s2);
         }
+        long RecursionFinalTime = ((System.nanoTime()- RecursionCurrentTime) / count);
+        System.out.println("Среднее время выполнения рекурсии с учетом запуска: " + count + " раз. Среднее время выполнения: " + RecursionFinalTime);
 
-    /*    System.out.println("Рекурсный и без кэша " + Recursion.computeLevenshteinDistance(s1, s2));
-        System.out.println("Время выполнения рекурсный и без кэша " + Recursion.difference);
-        System.out.println("Нерекурсивный с памятью " + LevenshteinMemory.computeLevenshteinDistanceDP(s1, s2));
-        System.out.println("Время выполнения без рекурсии " + LevenshteinMemory.difference);
-        int n= s1.length(), m = s2.length();
-       int[][] dp = new int[n + 1][m + 1];
-        for(int i = 0; i < n + 1; i++)
-            Arrays.fill(dp[i], -1);
-        System.out.println("Левенштейн с памятью и рекурсией " + RecursionWithMemory.minDis(s1, s2, n, m, dp));
-     //  System.out.println("Время выполнения с рекурсией с памятью " + RecursionWithMemory.timePass);
-      //  System.out.println("RecursionWithMemory.startTimestamp " + RecursionWithMemory.startTimestamp);
-      //  System.out.println("RecursionWithMemory.finishTimestamp " + RecursionWithMemory.finishTimestamp);
-      //  System.out.println("RecursionWithMemory.startInstant " + RecursionWithMemory.startInstant.toEpochMilli());
-      //  System.out.println("RecursionWithMemory.finishInstant) " + RecursionWithMemory.finishInstant.toEpochMilli());
-    */
+
+        long RecursionWithMemoryCurrentTime = System.nanoTime();
+        for (int i = 0 ; i <count; i++ ) {
+            RecursionWithMemory.minDis(s1, s2, s1.length(), s2.length(), dp);
+        }
+        long RecursionWithMemoryFinalTime = ((System.nanoTime()-RecursionWithMemoryCurrentTime) / count);
+        System.out.println("Среднее время выполнения рекурсии с использованием памяти и учетом запуска: " + count + " раз. Среднее время выполнения: " + RecursionWithMemoryFinalTime);
+
+
+        long LevenshteinMemoryCurrentTime = System.nanoTime();
+        for (int i = 0 ; i <count; i++ ) {
+            LevenshteinMemory.computeLevenshteinDistanceDP(s1, s2);
+        }
+        long LevenshteinMemoryFinalTime =((System.nanoTime()-LevenshteinMemoryCurrentTime) / count);
+        System.out.println("Среднее время выполнения с использованием памяти и учетом запуска: " + count + " раз. Среднее время выполнения: " + LevenshteinMemoryFinalTime);
+
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter("file.txt"));
+        bufferedWriter.write("Среднее время выполнения рекурсии с учетом запуска: " + count + " раз. Среднее время выполнения: " + RecursionFinalTime);
+        bufferedWriter.newLine();
+        bufferedWriter.flush();
+        bufferedWriter.write("Среднее время выполнения рекурсии с использованием памяти и учетом запуска: " + count + " раз. Среднее время выполнения: " + RecursionWithMemoryFinalTime);
+        bufferedWriter.newLine();
+        bufferedWriter.flush(); bufferedWriter.write("Среднее время выполнения с использованием памяти и учетом запуска: " + count + " раз. Среднее время выполнения: " + LevenshteinMemoryFinalTime);
+        bufferedWriter.newLine();
+        bufferedWriter.flush();
+        bufferedWriter.close();
+//        String file = "/home/avb/Documents/Учеба/Java_project/output.xlsx";
+//        String sheetname = "levenshtein";
+
     }
-
 }
